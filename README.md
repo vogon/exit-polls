88c635a3115e2aee28cf126b05d9a4466edf5591
vogon <vogon@outlook.com>
2017-03-22 23:36:53 -0700

    first working prototype

--- README.md
+++ README.md
@@ -1,37 +1,27 @@
-## Welcome to GitHub Pages
+<div id="graph"></div>
 
-You can use the [editor on GitHub](https://github.com/vogon/exit-polls/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
+<script type="application/javascript"
+	src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.7.3/d3.min.js"></script>
+<script type="application/javascript"
+	src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js"></script>
+<script type="application/javascript"
+	src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
-
-### Markdown
-
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
-
-```markdown
-Syntax highlighted code block
-
-# Header 1
-## Header 2
-### Header 3
-
-- Bulleted
-- List
-
-1. Numbered
-2. List
-
-**Bold** and _Italic_ and `Code` text
-
-[Link](url) and ![Image](src)
-```
-
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
-
-### Jekyll Themes
-
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/vogon/exit-polls/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
-
-### Support or Contact
-
-Having trouble with Pages? Check out our [documentation](https://help.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
+<script type="application/javascript" src="exit-polls.js"></script>
+<script type="x-tmpl-mustache" id="dataset-chooser-tmpl">
+	{% raw %}
+	<form id="dataset-chooser">
+		{{#datasets}}
+			<input id="{{id}}" type="checkbox" value="false">
+			<label for="{{id}}">
+				<svg width="20" height="20">
+					<g fill="none" stroke-width="3">
+						<path class="label-line" stroke="rgba(0,0,0,0)" d="M0,10L20,10"></path>
+					</g>
+				</svg>
+				{{friendly}}
+			</label>
+		{{/datasets}}
+	</form>
+	{% endraw %}
+</script>
---
+++ exit-polls.js
@@ -0,0 +1,201 @@
+/*
+ * I have stolen
+ * the colors
+ * that were in
+ * your blog post
+ *
+ * and which
+ * you were probably
+ * saving
+ * for products
+ *
+ * Forgive me
+ * they were pretty
+ * so accessible
+ * and so coordinated
+ *
+ * https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782
+ */
+var palette = [ "#4e79a7", "#f28e2b", "#e15759", "#76b7b2", "#59a14e",
+	"#edc949", "#b07aa2", "#ff9da7", "#9c755f", "#bab0ac" ];
+
+/*
+ * data from the Bureau of Labor Statistics:
+ * https://data.bls.gov/cgi-bin/cpicalc.pl
+ */
+var deflator1976 = 4.28124780;
+var deflator1980 = 2.95634709;
+var deflator1984 = 2.34459095;
+var deflator1988 = 2.05919696;
+var deflator1992 = 1.73630078;
+var deflator1996 = 1.55260038;
+var deflator2000 = 1.41465157;
+var deflator2004 = 1.28958708;
+var deflator2008 = 1.13144266;
+var deflator2012 = 1.06101640;
+var deflator2016 = 1.01498290;
+
+/*
+ * data from the Roper Center at Cornell:
+ * https://ropercenter.cornell.edu/polls/us-elections/how-groups-voted/how-groups-voted-2008/
+ * https://ropercenter.cornell.edu/polls/us-elections/how-groups-voted/how-groups-voted-2012/
+ * https://ropercenter.cornell.edu/groups-voted-2016/
+ */
+
+var clinton2016 = [
+	{ nominalLow: 0, pctileLow: 0, dem: 0.53, gop: 0.41 },
+	{ nominalLow: 50000, pctileLow: 0.36, dem: 0.46, gop: 0.49 },
+	{ nominalLow: 100000, pctileLow: 0.66, dem: 0.47, gop: 0.47 },
+	{ pctileLow: 1, dem: 0.47, gop: 0.47 }
+];
+
+var obama2012 = [
+	{ nominalLow: 0, pctileLow: 0, dem: 0.60, gop: 0.37 },
+	{ nominalLow: 50000, pctileLow: 0.41, dem: 0.46, gop: 0.52 },
+	{ nominalLow: 100000, pctileLow: 0.72, dem: 0.44, gop: 0.54 },
+	{ pctileLow: 1, dem: 0.44, gop: 0.54 }
+];
+
+var obama2008 = [
+	{ nominalLow: 0, pctileLow: 0, dem: 0.73, gop: 0.25 },
+	{ nominalLow: 15000, pctileLow: 0.06, dem: 0.60, gop: 0.37 },
+	{ nominalLow: 30000, pctileLow: 0.18, dem: 0.55, gop: 0.43 },
+	{ nominalLow: 50000, pctileLow: 0.37, dem: 0.48, gop: 0.49 },
+	{ nominalLow: 75000, pctileLow: 0.58, dem: 0.51, gop: 0.48 },
+	{ nominalLow: 100000, pctileLow: 0.73, dem: 0.49, gop: 0.49 },
+	{ pctileLow: 0.99, dem: 0.49, gop: 0.49 }
+];
+
+var kerry2004 = [
+	{ nominalLow: 0, pctileLow: 0,
+		dem: 0.63, gop: 0.36, ind1: 0.00, ind2: 0.00 },
+	{ nominalLow: 15000, pctileLow: 0.08,
+		dem: 0.57, gop: 0.42, ind1: 0.00, ind2: 0.01 },
+	{ nominalLow: 30000, pctileLow: 0.23,
+		dem: 0.50, gop: 0.49, ind1: 0.00, ind2: 0.00 },
+	{ nominalLow: 50000, pctileLow: 0.45,
+		dem: 0.43, gop: 0.56, ind1: 0.00, ind2: 0.01 },
+	{ nominalLow: 75000, pctileLow: 0.68,
+		dem: 0.45, gop: 0.55, ind1: 0.00, ind2: 0.01 },
+	{ nominalLow: 100000, pctileLow: 0.82,
+		dem: 0.41, gop: 0.58, ind1: 0.01, ind2: 0.00 },
+	{ pctileLow: 1, dem: 0.41, gop: 0.58 }
+];
+
+var datasets = [
+	{ id: "clinton2016", friendly: "Clinton 2016", data: clinton2016 },
+	{ id: "obama2012", friendly: "Obama 2012", data: obama2012 },
+	{ id: "obama2008", friendly: "Obama 2008", data: obama2008 },
+	{ id: "kerry2004", friendly: "Kerry 2004", data: kerry2004 }
+];
+
+var width = 800, height = 600;
+var xBorder = 40, yBorder = 40;
+
+var xScale = d3.scaleLinear()
+	.domain([0, 1])
+	.range([0, width - 2 * xBorder]);
+var yScale = d3.scaleLinear()
+	.domain([0.8, 0])
+	.range([0, height - 2 * yBorder]);
+
+var xAxis = d3.axisBottom(xScale)
+	.tickFormat(d3.format(".0%"));
+
+var yAxis = d3.axisLeft(yScale)
+	.tickFormat(d3.format(".0%"));
+
+var svg = d3.select("#graph").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+	
+svg.append("g")
+	.attr("transform", "translate(" + xBorder + ", " + (height - yBorder) 
+		+ ")")
+	.call(xAxis);
+
+svg.append("g")
+	.attr("transform", "translate(" + xBorder + ", " + yBorder + ")")
+	.call(yAxis);
+
+function drawDataset(dataset, dataset_id, color) {
+	var line = d3.line()
+		.x(function (datum) { return xScale(datum.pctileLow); })
+		.y(function (datum) { 
+			return yScale(datum.dem / (datum.dem + datum.gop));
+		})
+		.curve(d3.curveStepAfter);
+
+	svg.append("g")
+		.classed(dataset_id, true)
+		.attr("fill", "none")
+		.attr("stroke-width", 3)
+		.attr("transform", "translate(" + xBorder + ", " + yBorder + ")")
+		.append("path")
+		.datum(dataset)
+		.attr("stroke", color)
+		.attr("d", line);
+}
+
+var palettedDatasets = [];
+
+function showDataset(id) {
+	console.log("show! " + id);
+	var colorIdx = undefined;
+
+	for (var i = 0; i < 10; i++) {
+		if (palettedDatasets[i] === undefined) {
+			colorIdx = i;
+			palettedDatasets[colorIdx] = id;
+			break;
+		}
+	}
+
+	if (colorIdx === undefined) {
+		console.warn("couldn't find a color for " + id);
+		return;
+	}
+
+	datasets.forEach(function (dataset) {
+		if (dataset.id == id) {
+			drawDataset(dataset.data, id, palette[colorIdx]);
+		}
+	});
+
+	$("#dataset-chooser").find("label[for=\"" + id + "\"] .label-line")
+		.attr("stroke", palette[colorIdx]);
+}
+
+function hideDataset(id) {
+	console.log("hide! " + id);
+
+	for (var i = 0; i < palettedDatasets.length; i++) {
+		if (palettedDatasets[i] == id) {
+			delete palettedDatasets[i];
+		}
+	}
+
+	$("#graph g." + id).remove();
+
+	$("#dataset-chooser").find("label[for=\"" + id + "\"] .label-line")
+		.attr("stroke", "transparent");
+}
+
+$(function() {
+	var datasetChooserTemplate = $("#dataset-chooser-tmpl").html();
+	Mustache.parse(datasetChooserTemplate);
+
+	$("#graph").append(Mustache.render(datasetChooserTemplate,
+		{ datasets: datasets }));
+
+	$("#dataset-chooser").find("input[type=\"checkbox\"]")
+		.each(function (unused, element) {
+			$(element).change(function (e) {
+				if (e.target.checked) {
+					showDataset(e.target.id);
+				} else {
+					hideDataset(e.target.id);
+				}
+			});
+		});
+});
